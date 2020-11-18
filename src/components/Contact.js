f850adf9aa080ca0cc32c96d3a0ed61105c16a72
courtneyc148 <courtneyc148@gmail.com>
2020-11-18 11:04:45 -0500

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = (props) => {
     return (
         <div className='container'>
             <h4 className="center">Contact</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio minus debitis quo earum assumenda, vel voluptatem dolores dolore labore iure cum ducimus ad beatae perferendis nemo culpa modi corrupti accusamus dicta omnis incidunt possimus asperiores a. Temporibus delectus cupiditate laudantium quis ad fugiat voluptates blanditiis dolorem! Distinctio, officiis atque.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam distinctio minus debitis quo earum assumenda, vel voluptatem dolores dolore labore iure cum ducimus beatae perferendis nemo culpa modi corrupti accusamus dicta omnis incidunt possimus asperiores a. Temporibus delectus cupiditate laudantium quis ad fugiat voluptates blanditiis dolorem! Distinctio, officiis atque.</p>
         </div>
     )
 }
